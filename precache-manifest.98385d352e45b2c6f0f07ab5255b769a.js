37ab39a0b2b835bdbd6ca25ddff1eda7fba1391a
Geeeva <ivana.gilich@gmail.com>
2019-04-23 00:16:50 +0200

    Updates

---
+++ precache-manifest.98385d352e45b2c6f0f07ab5255b769a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9be895bb140c775055f8089c26a40350",
+    "url": "/Geeeva/Dining-room.git/static/media/cover.9be895bb.jpg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/Geeeva/Dining-room.git/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/Geeeva/Dining-room.git/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "bf2cf3894573bf5c075d9a377abf32d5",
+    "url": "/Geeeva/Dining-room.git/static/media/Inter-ExtraBold.bf2cf389.ttf"
+  },
+  {
+    "revision": "afa481d23570496ef317",
+    "url": "/Geeeva/Dining-room.git/static/js/runtime~main.db608bed.js"
+  },
+  {
+    "revision": "c90eb9b795fe4690a869",
+    "url": "/Geeeva/Dining-room.git/static/js/main.e6f9bf2e.chunk.js"
+  },
+  {
+    "revision": "a19c80921963fa54de89",
+    "url": "/Geeeva/Dining-room.git/static/js/2.380e6c34.chunk.js"
+  },
+  {
+    "revision": "c90eb9b795fe4690a869",
+    "url": "/Geeeva/Dining-room.git/static/css/main.1faf7a4e.chunk.css"
+  },
+  {
+    "revision": "a19c80921963fa54de89",
+    "url": "/Geeeva/Dining-room.git/static/css/2.f42f2e29.chunk.css"
+  },
+  {
+    "revision": "0220c463dc9e6c7ecddc008516c99c23",
+    "url": "/Geeeva/Dining-room.git/index.html"
+  }
+];
